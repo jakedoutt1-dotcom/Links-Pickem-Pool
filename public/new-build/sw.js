0a13d6f6d5ff74ef79fd60e2a5d5f09cc982b37d
jakedoutt1-dotcom <jakedoutt1@gmail.com>
2026-09-22 15:43:42 -0500

    v168 refresh cache for live slate rollover

--- public/new-build/sw.js
+++ public/new-build/sw.js
@@ -1,4 +1,4 @@
-const CACHE="links-new-build-shell-v167";
+const CACHE="links-new-build-shell-v168";
 const SHELL=["./","./index.html","./app.css","./app.js","./playmaker-splash.js","./reveal-guard.js","./playmaker-intro.mp3","./manifest.webmanifest"];
 self.addEventListener("install",event=>{
   self.skipWaiting();
