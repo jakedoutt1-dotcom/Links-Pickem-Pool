0afd7980bde8a66cd57db35adb46893bd89c3c4e
jakedoutt1-dotcom <jakedoutt1@gmail.com>
2026-09-22 15:29:09 -0500

    v157 refresh cache after demo quarantine

--- public/new-build/sw.js
+++ public/new-build/sw.js
@@ -1,4 +1,4 @@
-const CACHE="links-new-build-shell-v156";
+const CACHE="links-new-build-shell-v157";
 const SHELL=["./","./index.html","./app.css","./app.js","./playmaker-splash.js","./reveal-guard.js","./playmaker-intro.mp3","./manifest.webmanifest"];
 self.addEventListener("install",event=>{
   self.skipWaiting();
