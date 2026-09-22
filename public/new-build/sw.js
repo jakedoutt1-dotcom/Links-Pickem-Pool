4b78731cb52eae4a936ee84c6e4aff0000babbb5
jakedoutt1-dotcom <jakedoutt1@gmail.com>
2026-09-22 15:32:18 -0500

    v160 refresh cache for NFL college live data

--- public/new-build/sw.js
+++ public/new-build/sw.js
@@ -1,4 +1,4 @@
-const CACHE="links-new-build-shell-v159";
+const CACHE="links-new-build-shell-v160";
 const SHELL=["./","./index.html","./app.css","./app.js","./playmaker-splash.js","./reveal-guard.js","./playmaker-intro.mp3","./manifest.webmanifest"];
 self.addEventListener("install",event=>{
   self.skipWaiting();
