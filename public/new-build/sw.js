34ffefe26479d61ca2f0c9c84e2cc70a39be56d1
jakedoutt1-dotcom <jakedoutt1@gmail.com>
2026-09-22 15:40:33 -0500

    v165 refresh cache for reveal finish

--- public/new-build/sw.js
+++ public/new-build/sw.js
@@ -1,4 +1,4 @@
-const CACHE="links-new-build-shell-v164";
+const CACHE="links-new-build-shell-v165";
 const SHELL=["./","./index.html","./app.css","./app.js","./playmaker-splash.js","./reveal-guard.js","./playmaker-intro.mp3","./manifest.webmanifest"];
 self.addEventListener("install",event=>{
   self.skipWaiting();
