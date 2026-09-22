81d49b81f6c0107c22c8fadf31e56d31aa90482d
jakedoutt1-dotcom <jakedoutt1@gmail.com>
2026-09-22 15:17:20 -0500

    v149 cache Playmaker splash and intro audio

--- public/new-build/sw.js
+++ public/new-build/sw.js
@@ -1,5 +1,5 @@
-const CACHE="links-new-build-shell-v146";
-const SHELL=["./","./index.html","./app.css","./app.js","./manifest.webmanifest"];
+const CACHE="links-new-build-shell-v149";
+const SHELL=["./","./index.html","./app.css","./app.js","./playmaker-splash.js","./playmaker-intro.mp3","./manifest.webmanifest"];
 self.addEventListener("install",event=>{
   self.skipWaiting();
   event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(SHELL)));
