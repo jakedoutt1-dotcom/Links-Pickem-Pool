9c793223e9f96d0658ade69778cf94e1ed7f1e52
jakedoutt1-dotcom <jakedoutt1@gmail.com>
2026-09-22 15:50:03 -0500

    v173 cache seed purge

--- public/new-build/sw.js
+++ public/new-build/sw.js
@@ -1,4 +1,4 @@
-const CACHE="links-new-build-shell-v172";
+const CACHE="links-new-build-shell-v173";
 const SHELL=["./","./index.html","./app.css","./app.js","./playmaker-splash.js","./reveal-guard.js","./playmaker-intro.mp3","./manifest.webmanifest"];
 self.addEventListener("install",event=>{
   self.skipWaiting();
