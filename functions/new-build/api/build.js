ef4111f9e752107369c22d809ca4e752ee624336
jakedoutt1-dotcom <jakedoutt1@gmail.com>
2026-09-23 16:49:12 -0500

    Bump New Build to v485 after cache repair

--- functions/new-build/api/build.js
+++ functions/new-build/api/build.js
@@ -1,1 +1,1 @@
-export function onRequestGet(){return Response.json({build:"484"},{headers:{"Cache-Control":"no-store, no-cache, must-revalidate","Pragma":"no-cache","Expires":"0"}})}
+export function onRequestGet(){return Response.json({build:"485"},{headers:{"Cache-Control":"no-store, no-cache, must-revalidate","Pragma":"no-cache","Expires":"0"}})}
