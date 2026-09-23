4cc81290f5be0eebc7170e161ae437d990a25cb0
jakedoutt1-dotcom <jakedoutt1@gmail.com>
2026-09-23 16:44:00 -0500

    Sync football live-score week update v483

--- functions/new-build/api/build.js
+++ functions/new-build/api/build.js
@@ -1,1 +1,1 @@
-export function onRequestGet(){return Response.json({build:"482"},{headers:{"Cache-Control":"no-store, no-cache, must-revalidate","Pragma":"no-cache","Expires":"0"}})}
+export function onRequestGet(){return Response.json({build:"483"},{headers:{"Cache-Control":"no-store, no-cache, must-revalidate","Pragma":"no-cache","Expires":"0"}})}
