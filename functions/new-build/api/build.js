8fb6702a31fdcbf94942c2c21628b6756a4b418b
jakedoutt1-dotcom <jakedoutt1@gmail.com>
2026-09-23 10:15:47 -0500

    v410 make build API authoritative

--- functions/new-build/api/build.js
+++ functions/new-build/api/build.js
@@ -1,1 +1,1 @@
-export function onRequestGet(){return Response.json({build:"396"},{headers:{"Cache-Control":"no-store, no-cache, must-revalidate","Pragma":"no-cache","Expires":"0"}})}
+export function onRequestGet(){return Response.json({build:"410"},{headers:{"Cache-Control":"no-store, no-cache, must-revalidate","Pragma":"no-cache","Expires":"0"}})}
