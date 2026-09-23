2632a347c7343918ff768071d725f4545915c0aa
jakedoutt1-dotcom <jakedoutt1@gmail.com>
2026-09-23 16:21:58 -0500

    Sync New Build API version to v480

--- functions/new-build/api/build.js
+++ functions/new-build/api/build.js
@@ -1,1 +1,1 @@
-export function onRequestGet(){return Response.json({build:"415"},{headers:{"Cache-Control":"no-store, no-cache, must-revalidate","Pragma":"no-cache","Expires":"0"}})}
+export function onRequestGet(){return Response.json({build:"480"},{headers:{"Cache-Control":"no-store, no-cache, must-revalidate","Pragma":"no-cache","Expires":"0"}})}
