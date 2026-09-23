0a010eca235caa6c2d3d6d8a5c76b50676772087
jakedoutt1-dotcom <jakedoutt1@gmail.com>
2026-09-23 17:16:12 -0500

    Bump New Build to v489

--- functions/new-build/api/build.js
+++ functions/new-build/api/build.js
@@ -1,1 +1,1 @@
-export function onRequestGet(){return Response.json({build:"488"},{headers:{"Cache-Control":"no-store, no-cache, must-revalidate","Pragma":"no-cache","Expires":"0"}})}
+export function onRequestGet(){return Response.json({build:"489"},{headers:{"Cache-Control":"no-store, no-cache, must-revalidate","Pragma":"no-cache","Expires":"0"}})}
