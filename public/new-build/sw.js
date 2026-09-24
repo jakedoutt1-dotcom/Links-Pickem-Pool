1c61d5f9b551c7d349adc94d331f6fbda76e51d6
jakedoutt1-dotcom <jakedoutt1@gmail.com>
2026-09-24 11:50:31 -0500

    New Build v560 game picker navigation

--- public/new-build/sw.js
+++ public/new-build/sw.js
@@ -1,6 +1,6 @@
-const CACHE="links-new-build-shell-v559";
+const CACHE="links-new-build-shell-v560";
 const SHELL=["./","./index.html","./app.css","./app.js","./build.js","./shell.css","./shell.js","./manifest.webmanifest"];
-self.addEventListener("install",event=>{self.skipWaiting();event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(SHELL.map(x=>x+"?v=559")).catch(()=>{}))});
+self.addEventListener("install",event=>{self.skipWaiting();event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(SHELL.map(x=>x+"?v=560")).catch(()=>{}))});
 self.addEventListener("activate",event=>{event.waitUntil(Promise.all([caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))),self.clients.claim()]))});
 self.addEventListener("fetch",event=>{if(event.request.method!=="GET")return;const url=new URL(event.request.url);if(url.origin!==self.location.origin)return;
  if(event.request.mode==="navigate"||url.pathname.includes("/new-build/api/")||/\.(?:html|js|css)$/.test(url.pathname)){event.respondWith(fetch(event.request,{cache:"no-store"}).catch(()=>caches.match(event.request)));return}
