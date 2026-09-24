a69877914d07e485af6c84bed71cc1f3e29647ff
jakedoutt1-dotcom <jakedoutt1@gmail.com>
2026-09-24 07:46:21 -0500

    Sync New Build v522 community image framing

--- functions/new-build/api/build.js
+++ functions/new-build/api/build.js
@@ -1,1 +1,1 @@
-export function onRequestGet(){return Response.json({build:"521"},{headers:{"Cache-Control":"no-store, no-cache, must-revalidate","Pragma":"no-cache","Expires":"0"}})}
+export function onRequestGet(){return Response.json({build:"522"},{headers:{"Cache-Control":"no-store, no-cache, must-revalidate","Pragma":"no-cache","Expires":"0"}})}
