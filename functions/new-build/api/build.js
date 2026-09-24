b1c3fca31b73ba7feb0da44cd413e404dc943d89
jakedoutt1-dotcom <jakedoutt1@gmail.com>
2026-09-23 23:50:25 -0500

    Bump New Build to v509 for NFL College Playmaker odds sync

--- functions/new-build/api/build.js
+++ functions/new-build/api/build.js
@@ -1,1 +1,1 @@
-export function onRequestGet(){return Response.json({build:"508"},{headers:{"Cache-Control":"no-store, no-cache, must-revalidate","Pragma":"no-cache","Expires":"0"}})}
+export function onRequestGet(){return Response.json({build:"509"},{headers:{"Cache-Control":"no-store, no-cache, must-revalidate","Pragma":"no-cache","Expires":"0"}})}
