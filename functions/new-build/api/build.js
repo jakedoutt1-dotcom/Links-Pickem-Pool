6ec8e34b2c9a405d7be7a0e2574069c8d0af8d17
jakedoutt1-dotcom <jakedoutt1@gmail.com>
2026-09-24 07:16:09 -0500

    Sync New Build v515 session gate

--- functions/new-build/api/build.js
+++ functions/new-build/api/build.js
@@ -1,1 +1,1 @@
-export function onRequestGet(){return Response.json({build:"514"},{headers:{"Cache-Control":"no-store, no-cache, must-revalidate","Pragma":"no-cache","Expires":"0"}})}
+export function onRequestGet(){return Response.json({build:"515"},{headers:{"Cache-Control":"no-store, no-cache, must-revalidate","Pragma":"no-cache","Expires":"0"}})}
