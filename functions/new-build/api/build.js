6cefec5d19a35cc3153136e95e6007e74bfd9c19
jakedoutt1-dotcom <jakedoutt1@gmail.com>
2026-09-24 08:20:25 -0500

    Sync New Build v528 splash experience

--- functions/new-build/api/build.js
+++ functions/new-build/api/build.js
@@ -1,1 +1,1 @@
-export function onRequestGet(){return Response.json({build:"527"},{headers:{"Cache-Control":"no-store, no-cache, must-revalidate","Pragma":"no-cache","Expires":"0"}})}
+export function onRequestGet(){return Response.json({build:"528"},{headers:{"Cache-Control":"no-store, no-cache, must-revalidate","Pragma":"no-cache","Expires":"0"}})}
