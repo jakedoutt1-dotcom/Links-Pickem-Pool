45a4f34642324e53f0d4b9cf99e17bfa6f6ef28c
jakedoutt1-dotcom <jakedoutt1@gmail.com>
2026-09-23 23:48:14 -0500

    Bump New Build to v508 for normalized odds response

--- functions/new-build/api/build.js
+++ functions/new-build/api/build.js
@@ -1,1 +1,1 @@
-export function onRequestGet(){return Response.json({build:"507"},{headers:{"Cache-Control":"no-store, no-cache, must-revalidate","Pragma":"no-cache","Expires":"0"}})}
+export function onRequestGet(){return Response.json({build:"508"},{headers:{"Cache-Control":"no-store, no-cache, must-revalidate","Pragma":"no-cache","Expires":"0"}})}
